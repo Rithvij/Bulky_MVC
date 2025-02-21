2577b76c3ac09dd0bf39b35fc366b95b3ac6171c
r134p694_a <rithvij2019@gmail.com>
2025-02-21 10:54:16 -0600

    post deployment changes

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -17,7 +17,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>
     (options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
 builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.ConfigureApplicationCookie(options =>
 {
