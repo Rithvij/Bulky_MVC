16cb96fe90c94c5bf61de783b6d98b202753e272
r134p694_a <rithvij2019@gmail.com>
2025-02-21 10:55:07 -0600

    post deployment changes

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
-using Microsoft.Build.Framework;
+//using Microsoft.Build.Framework;
 using Stripe;
 using BulkyBook.DataAccess.DbInitializer;
 
