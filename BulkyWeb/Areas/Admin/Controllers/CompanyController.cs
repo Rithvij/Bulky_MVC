eb00efa5f697764435d7c5bdf74e204fc743e4d7
r134p694_a <rithvij2019@gmail.com>
2025-02-20 23:15:12 -0600

    Pushing before deployment - project completion

---
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,171 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+       
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            
+        }
+        public IActionResult Index()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+
+            return View(objCompanyList);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company CompanyObj)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                
+                
+                if (CompanyObj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(CompanyObj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(CompanyObj);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Company Created Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                
+                return View(CompanyObj);
+            }
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            //CompanyVM CompanyVM = new()
+            //{
+            //    CategoryList = _unitOfWork.Category.GetAll()
+            //    .Select(u => new SelectListItem
+            //    {
+            //        Text = u.Name,
+            //        Value = u.Id.ToString()
+            //    }),
+            //    Company = new Company()
+            //};
+            if (id == null || id == 0)
+            {
+                return View(new Company());
+            }
+            else
+            {
+                Company companyObj = _unitOfWork.Company.Get(u => u.Id == id);
+                return View(companyObj);
+            }
+
+        }
+
+        //[HttpPost]
+        //public IActionResult Edit(Company obj)
+        //{
+        //    //if (obj.Name == obj.DisplayOrder.ToString())
+        //    //{
+        //    //    ModelState.AddModelError("name", "The Display Order cannot exactly match the name.");
+        //    //}
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Company.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Company Updated Successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Company? CompanyFromDb = _unitOfWork.Company.Get(u => u.Id == id);
+        //    //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+        //    //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id==id).FirstOrDefault();
+        //    if (CompanyFromDb == null) return NotFound();
+
+        //    return View(CompanyFromDb);
+        //}
+
+        //[HttpPost, ActionName("Delete")]
+        //public IActionResult DeletePost(int? id)
+        //{
+        //    Company? obj = _unitOfWork.Company.Get(u => u.Id == id);
+        //    if (obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    _unitOfWork.Company.Remove(obj);
+        //    _unitOfWork.Save();
+        //    TempData["success"] = "Company Deleted Successfully";
+        //    return RedirectToAction("Index");
+
+        //}
+
+        //public IActionResult Delete(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Company? CompanyFromDb = _unitOfWork.Company.Get(u => u.Id == id);
+        //    //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+        //    //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id==id).FirstOrDefault();
+        //    if (CompanyFromDb == null) return NotFound();
+
+        //    return View(CompanyFromDb);
+        //}
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+
+            return Json(new { data = objCompanyList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var CompanyToBeDeleted = _unitOfWork.Company.Get(u=>u.Id == id);
+            if(CompanyToBeDeleted==null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            
+            _unitOfWork.Company.Remove(CompanyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success=true, message="Delete Successful" });
+        }
+        #endregion
+
+    }
+}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,5 +1,14 @@
-using BulkyWeb.Data;
+using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+//using BulkyBook.DataAccess.Repository.CategoryRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
+using Microsoft.Build.Framework;
+using Stripe;
+using BulkyBook.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +17,34 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>
     (options => options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+builder.Services.AddAuthentication().AddFacebook(option =>
+{
+    option.AppId = "2043357879503539";
+    option.AppSecret = "1acb79349dd8984aef9277bef4115d51";
+});
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
+builder.Services.AddScoped<IDbInitializer,DbInitializer>();
+builder.Services.AddRazorPages();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -22,13 +54,23 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseSession();
+SeedDatabase();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
